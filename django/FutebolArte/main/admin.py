81adebc7a42c1f5891cd84df6037440c0483fbdb
Maciel Alves Pereira <macielalves.dev@gmail.com>
2024-02-23 08:21:21 -0300

    feat: inline add

--- django/FutebolArte/main/admin.py
+++ django/FutebolArte/main/admin.py
@@ -2,19 +2,22 @@ from django.contrib import admin
 from .models import Jogador, ClubeTime, Competicao, TituloCompeticao
 
 
-@admin.register(Jogador)
-class JogadorAdmin(admin.ModelAdmin):
-    list_display = ["nome"]
-    list_filter = ["clube"]
+class JogadorInline(admin.StackedInline):
+    model = Jogador
+
+
+class TituloCompeticaoInline(admin.TabularInline):
+    model = TituloCompeticao
 
 
 @admin.register(ClubeTime)
-class ClubeTimeAdmin(admin.ModelAdmin): ...
+class ClubeTimeAdmin(admin.ModelAdmin):
+    inlines = [JogadorInline, TituloCompeticaoInline]
 
 
 @admin.register(Competicao)
 class CompeticaoAdmin(admin.ModelAdmin): ...
 
 
-@admin.register(TituloCompeticao)
-class TituloCompeticaoAdmin(admin.ModelAdmin): ...
+admin.site.register(Jogador)
+admin.site.register(TituloCompeticao)
